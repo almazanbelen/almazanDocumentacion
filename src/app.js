07a06562441ba3becc47cbc36029ea2443974993
Belen Almazan <almazanbelen01@gmail.com>
2023-12-19 12:54:25 -0300

    practica integradora terminada

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(config.mongoURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
